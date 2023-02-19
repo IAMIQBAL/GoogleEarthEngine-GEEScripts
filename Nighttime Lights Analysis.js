d973e6ee5d5bb8f46b2736d38263873439ad8cd5
mib <muhammadiqbal2015@hotmail.com>
2023-02-19 22:36:00 +0500

    first commit

---
+++ Nighttime Lights Analysis.js
@@ -0,0 +1,31 @@
+// RGB Composite of Night Lights using Additive Color System
+// 2013: Red
+// 2003: Green
+// 1993: Blue
+
+// Enter ROI [West, South, East, North]
+var ROI = [59.545898,23.155933,79.541016,37.439499];
+
+// More coverages https://www.ncei.noaa.gov/products/dmsp-operational-linescan-system
+var yearR = 'F182013'
+var yearG = 'F182012'
+var yearB = 'F182011'
+
+var nB = ee.Image('NOAA/DMSP-OLS/NIGHTTIME_LIGHTS/' + yearB);
+var nG = ee.Image('NOAA/DMSP-OLS/NIGHTTIME_LIGHTS/' + yearG)
+                  .select('stable_lights')
+                  .rename(yearG)
+                  
+var nR = ee.Image('NOAA/DMSP-OLS/NIGHTTIME_LIGHTS/' + yearR)
+                  .select('stable_lights')
+                  .rename(yearR)
+                  
+var composite = nR.addBands(nG)
+                      .addBands(nB.select('stable_lights')
+                      .rename(yearB))
+
+var bbox = ee.Geometry.BBox(ROI[0], ROI[1], ROI[2], ROI[3]);
+
+var clipNL = composite.clip(bbox)
+Map.addLayer(clipNL, {min:0,max:63}, "NightLights");
+Map.centerObject(clipNL, 3);
